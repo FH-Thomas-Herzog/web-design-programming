aa686ea19d9ad3431141797cafaed95c636a1e72
cchet <herzog.thomas81@gmail.com>
2014-11-22 09:41:56 +0100

    lab project created

---
+++ Semester-3/20141122_lab/quicksort/code.js
@@ -0,0 +1,20 @@
+console.log("Code.js loaded")
+
+$(init)
+
+function init() {
+    
+}
+
+function gameLoop() {
+    update()
+    draw()
+}
+
+function update() {
+    
+}
+
+function draw() {
+    
+}
